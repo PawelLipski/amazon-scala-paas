f1e1d67b5c92cec2605d140ca3f9771054879e95
Pawel Lipski <pawel.p.lipski@gmail.com>
2015-04-30 05:16:07 +0200

    Create a playbook for master boot

--- boot.sh
+++ boot.sh
@@ -9,7 +9,7 @@ MASTER_USER_HOST=ubuntu@$1
 PRIVATE_KEY_FILE=$2
 MASTER_BOOT_DIR=/home/ubuntu/paas-boot
 
-ssh -i $PRIVATE_KEY_FILE $MASTER_USER_HOST "mkdir -p $MASTER_BOOT_DIR"
-scp -i $PRIVATE_KEY_FILE ansible_hosts slave-start.yml slave-stop.yml $MASTER_USER_HOST:$MASTER_BOOT_DIR
-#ssh $MASTER_USER_HOST "ansible-playbook -i $MASTER_BOOT_DIR/ansible_hosts $MASTER_BOOT_DIR/start.yml"
+ansible-playbook -i ansible_hosts master-start.yml
+scp -i $PRIVATE_KEY_FILE ansible_hosts slaves-start.yml slaves-stop.yml $MASTER_USER_HOST:$MASTER_BOOT_DIR
+#ssh $MASTER_USER_HOST "ansible-playbook -i $MASTER_BOOT_DIR/ansible_hosts $MASTER_BOOT_DIR/slaves-start.yml"
 
