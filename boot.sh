b7552c0e4823946a17c76edd2f76cb8691d290af
Pawel Lipski <pawel.p.lipski@gmail.com>
2015-05-01 04:46:39 +0200

    Preprocess ansible_hosts

--- boot.sh
+++ boot.sh
@@ -5,11 +5,13 @@ if [ $# -ne 2 ]; then
 	exit 1
 fi
 
-MASTER_USER_HOST=ubuntu@$1
+export MASTER_PUBLIC_IP=$1
+MASTER_USER_HOST=ubuntu@$MASTER_PUBLIC_IP
 PRIVATE_KEY_FILE=$2
 MASTER_BOOT_DIR=/home/ubuntu/paas-boot
 
+sed "s/MASTER_PUBLIC_IP/$MASTER_PUBLIC_IP/g" ansible_hosts.pattern > ansible_hosts
 ansible-playbook -i ansible_hosts master-start.yml
 scp -i $PRIVATE_KEY_FILE ansible_hosts slaves-start.yml slaves-stop.yml $MASTER_USER_HOST:$MASTER_BOOT_DIR
-#ssh $MASTER_USER_HOST "ansible-playbook -i $MASTER_BOOT_DIR/ansible_hosts $MASTER_BOOT_DIR/slaves-start.yml"
+#ssh -i $PRIVATE_KEY_FILE $MASTER_USER_HOST "ansible-playbook -i $MASTER_BOOT_DIR/ansible_hosts $MASTER_BOOT_DIR/slaves-start.yml"
 
