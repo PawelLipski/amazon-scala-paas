ad73e1b986105a7088c1f9baabeba7e3d1b34adc
Pawel Lipski <pawel.p.lipski@gmail.com>
2015-05-01 14:57:55 +0200

    Disable host key checking on master

--- boot.sh
+++ boot.sh
@@ -13,7 +13,7 @@ MASTER_REPO_DIR=/home/ubuntu/paas-repo
 sed -e "s/MASTER_PUBLIC_IP/$MASTER_PUBLIC_IP/g" -e "s!PRIVATE_KEY_FILE!$PRIVATE_KEY_FILE!g" ansible_hosts_control.pattern > ansible_hosts_control
 #chmod a-x ansible_hosts
 ansible-playbook -i ansible_hosts_control master-start.yml
-scp -i $PRIVATE_KEY_FILE $PRIVATE_KEY_FILE $MASTER_USER_HOST:/home/ubuntu/
+scp -i $PRIVATE_KEY_FILE $PRIVATE_KEY_FILE $MASTER_USER_HOST:/home/ubuntu/ 2>/dev/null
 #ssh -i $PRIVATE_KEY_FILE $MASTER_USER_HOST "chmod a-x $MASTER_BOOT_DIR/ansible_hosts"
 ssh -i $PRIVATE_KEY_FILE $MASTER_USER_HOST "ansible-playbook -i $MASTER_REPO_DIR/ansible_hosts_master $MASTER_REPO_DIR/slaves-start.yml"
 
