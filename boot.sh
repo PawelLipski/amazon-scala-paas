5398f21b2410c205e4a3dfa50ce31cf1eeec0f19
Pawel Lipski <pawel.p.lipski@gmail.com>
2015-05-01 14:36:01 +0200

    Set up master boot files git-wise

--- boot.sh
+++ boot.sh
@@ -5,15 +5,15 @@ if [ $# -ne 2 ]; then
 	exit 1
 fi
 
-export MASTER_PUBLIC_IP=$1
+MASTER_PUBLIC_IP=$1
 MASTER_USER_HOST=ubuntu@$MASTER_PUBLIC_IP
 PRIVATE_KEY_FILE=$2
-MASTER_BOOT_DIR=/home/ubuntu/paas-boot
+MASTER_REPO_DIR=/home/ubuntu/paas-repo
 
-sed -e "s/MASTER_PUBLIC_IP/$MASTER_PUBLIC_IP/g" -e "s!PRIVATE_KEY_FILE!$PRIVATE_KEY_FILE!g" ansible_hosts.pattern > ansible_hosts
-chmod a-x ansible_hosts
-ansible-playbook -i ansible_hosts master-start.yml
-scp -i $PRIVATE_KEY_FILE ansible_hosts slaves-start.yml slaves-stop.yml $MASTER_USER_HOST:$MASTER_BOOT_DIR
-ssh -i $PRIVATE_KEY_FILE $MASTER_USER_HOST "chmod a-x $MASTER_BOOT_DIR/ansible_hosts"
-ssh -i $PRIVATE_KEY_FILE $MASTER_USER_HOST "ansible-playbook -i $MASTER_BOOT_DIR/ansible_hosts $MASTER_BOOT_DIR/slaves-start.yml"
+sed -e "s/MASTER_PUBLIC_IP/$MASTER_PUBLIC_IP/g" -e "s!PRIVATE_KEY_FILE!$PRIVATE_KEY_FILE!g" ansible_hosts_control.pattern > ansible_hosts_control
+#chmod a-x ansible_hosts
+ansible-playbook -i ansible_hosts_control master-start.yml
+scp -i $PRIVATE_KEY_FILE $PRIVATE_KEY_FILE $MASTER_USER_HOST:/home/ubuntu/
+#ssh -i $PRIVATE_KEY_FILE $MASTER_USER_HOST "chmod a-x $MASTER_BOOT_DIR/ansible_hosts"
+ssh -i $PRIVATE_KEY_FILE $MASTER_USER_HOST "ansible-playbook -i $MASTER_REPO_DIR/ansible_hosts_master $MASTER_REPO_DIR/slaves-start.yml"
 
