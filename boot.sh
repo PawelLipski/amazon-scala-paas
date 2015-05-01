bd48267b186af322b81a24a541b14c3c7a1fbc5a
Pawel Lipski <pawel.p.lipski@gmail.com>
2015-05-01 05:01:26 +0200

    Care for ansible_hosts un-executability

--- boot.sh
+++ boot.sh
@@ -11,7 +11,9 @@ PRIVATE_KEY_FILE=$2
 MASTER_BOOT_DIR=/home/ubuntu/paas-boot
 
 sed -e "s/MASTER_PUBLIC_IP/$MASTER_PUBLIC_IP/g" -e "s!PRIVATE_KEY_FILE!$PRIVATE_KEY_FILE!g" ansible_hosts.pattern > ansible_hosts
+chmod a-x ansible_hosts
 ansible-playbook -i ansible_hosts master-start.yml
 scp -i $PRIVATE_KEY_FILE ansible_hosts slaves-start.yml slaves-stop.yml $MASTER_USER_HOST:$MASTER_BOOT_DIR
-#ssh -i $PRIVATE_KEY_FILE $MASTER_USER_HOST "ansible-playbook -i $MASTER_BOOT_DIR/ansible_hosts $MASTER_BOOT_DIR/slaves-start.yml"
+ssh -i $PRIVATE_KEY_FILE $MASTER_USER_HOST "chmod a-x $MASTER_BOOT_DIR/ansible_hosts"
+ssh -i $PRIVATE_KEY_FILE $MASTER_USER_HOST "ansible-playbook -i $MASTER_BOOT_DIR/ansible_hosts $MASTER_BOOT_DIR/slaves-start.yml"
 
