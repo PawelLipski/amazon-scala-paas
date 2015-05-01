0a0d6a79bb64649bc454dba51bd46bc5da1fb3bf
Pawel Lipski <pawel.p.lipski@gmail.com>
2015-05-01 04:51:17 +0200

    Set up PRIVATE_KEY_FILE in inventory via command line

--- boot.sh
+++ boot.sh
@@ -10,7 +10,7 @@ MASTER_USER_HOST=ubuntu@$MASTER_PUBLIC_IP
 PRIVATE_KEY_FILE=$2
 MASTER_BOOT_DIR=/home/ubuntu/paas-boot
 
-sed "s/MASTER_PUBLIC_IP/$MASTER_PUBLIC_IP/g" ansible_hosts.pattern > ansible_hosts
+sed -e "s/MASTER_PUBLIC_IP/$MASTER_PUBLIC_IP/g" -e "s!PRIVATE_KEY_FILE!$PRIVATE_KEY_FILE!g" ansible_hosts.pattern > ansible_hosts
 ansible-playbook -i ansible_hosts master-start.yml
 scp -i $PRIVATE_KEY_FILE ansible_hosts slaves-start.yml slaves-stop.yml $MASTER_USER_HOST:$MASTER_BOOT_DIR
 #ssh -i $PRIVATE_KEY_FILE $MASTER_USER_HOST "ansible-playbook -i $MASTER_BOOT_DIR/ansible_hosts $MASTER_BOOT_DIR/slaves-start.yml"
