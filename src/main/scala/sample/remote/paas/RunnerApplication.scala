83d105de1d50baaf5b491720b03ea80c7bb5bb47
BartoszJanota <bartosz.janota@gmail.com>
2015-05-21 11:09:32 +0200

    Master  IP address added for slave runner script

--- src/main/scala/sample/remote/paas/RunnerApplication.scala
+++ src/main/scala/sample/remote/paas/RunnerApplication.scala
@@ -10,8 +10,13 @@ object RunnerApplication {
   def main(args: Array[String]): Unit = {
     if (args.isEmpty || args.head == "Master")
       startRemoteMasterSystem()
-    if (args.isEmpty || args.head == "Slave")
-      startRemoteSlaveSystem()
+    if (args.isEmpty || args.head == "Slave"){
+      if (args.length > 1){
+        startRemoteSlaveSystem(args(1))
+      } else {
+        startRemoteSlaveSystem("127.0.0.1")
+      }
+    }
   }
 
   def startRemoteMasterSystem(): Unit = {
@@ -22,16 +27,17 @@ object RunnerApplication {
     println("Started MasterSystem - waiting for messages")
   }
 
-  def startRemoteSlaveSystem(): Unit = {
+  def startRemoteSlaveSystem(masterIP: String): Unit = {
     val system =
       ActorSystem("SlaveSystem", ConfigFactory.load("slave"))
     val remoteMasterPath =
-      "akka.tcp://MasterSystem@127.0.0.1:2552/user/master"
+      "akka.tcp://MasterSystem@" + masterIP + ":2552/user/master"
     val actor = system.actorOf(Props(classOf[SlaveActor], remoteMasterPath), "slave")
 
     println("Started SlaveSystem")
     import system.dispatcher
     system.scheduler.schedule(1.second, 5.second) {
+      println("###Master, tell me something interesting please!\n")
       actor ! TellMeSomethingMyMaster()
     }
   }
