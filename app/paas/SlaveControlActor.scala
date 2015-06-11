4b0dd66bfd169c567fe32aef0f29df8cf5439864
aksiazek <alexander.ksiazek@gmail.com>
2015-06-11 16:15:17 +0200

    debugging

--- app/paas/SlaveControlActor.scala
+++ app/paas/SlaveControlActor.scala
@@ -27,8 +27,8 @@ class SlaveControlActor(masterPath: String) extends Actor {
       Logger.info(agentSpec.toString)
       val agents = agentSpec.map(agent => 
         (context.actorOf(
-            Props(Class.forName(agent._1)),
-            	agent._1.split(".").last+agent._2), agent._2))
+            Props(Class.forName(agent._1)), "s"), agent._2))
+            	//agent._1.split(".").last+agent._2)
             
             
       for(agent <- agents)
